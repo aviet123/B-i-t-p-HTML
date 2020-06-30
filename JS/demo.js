2be568a7df9b55539da377d96e4e3877cfb9d566
dinh viet <dinhv2388@gmail.com>
2020-06-30 09:16:00 +0700

    demojs2

---
+++ JS/demo.js
@@ -0,0 +1,15 @@
+function showmess1() {
+    alert('xin chào')
+
+}
+function showmess2() {
+    document.getElementById('messdiv').innerText = 'xin chao'
+
+}
+function showmess3() {
+    document.write('xin chaogit ')
+
+}
+function showmess() {
+    console.log('xin chao')
+}
